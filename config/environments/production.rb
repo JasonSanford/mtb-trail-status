4d692a6f2fbb88104b3980228b6b097479a9d280
Jason Sanford <jasonsanford@gmail.com>
2015-11-18 21:54:30 -0500

    Add Mandrill credz to production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,13 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'mtbtrailstat.us' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { host: 'smtp.mandrillapp.com', port: 587,
+                                        password: ENV['MANDRILL_API_TOKEN'],
+                                        user_name: 'jasonsanford@gmail.com'}
+  config.action_mailer.asset_host = 'http://mtbtrailstat.us'
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
