ede49d148a2da78872111643989d4a6ba0f28f64
Jason Sanford <jasonsanford@gmail.com>
2016-05-10 16:15:20 -0400

    Remove geojson_url column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510195428) do
+ActiveRecord::Schema.define(version: 20160510201239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20160510195428) do
     t.string   "source",                                               null: false
     t.string   "display_name"
     t.string   "status"
-    t.string   "geojson_url"
     t.datetime "created_at",                                           null: false
     t.datetime "updated_at",                                           null: false
     t.text     "weather_json"
